f40ada3d32bb074856e4c71661793a1ae5bcbcf0
rangrik <pranav@shoja.app>
2024-07-11 03:37:30 +0530

    updated page layout
    for protected pages

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,4 +1,5 @@
 import AppHeader from "@/components/AppHeader";
+import AppNavbar from "@/components/AppNavbar";
 import AuthProvider from "@/supertokens/authProvider";
 
 export default function ProtectedAppLayout({
@@ -8,9 +9,12 @@ export default function ProtectedAppLayout({
 }>) {
   return (
     <AuthProvider>
-      <main className="p-2">
+      <main className="h-screen p-2 flex flex-col">
         <AppHeader />
-        {children}
+        <div className="flex grow mt-2">
+          <AppNavbar />
+          <div className="grow">{children}</div>
+        </div>
       </main>
     </AuthProvider>
   );
--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -1,14 +1,14 @@
-import { Button } from "@mantine/core";
+import ZoomIntegrationCard from "@/integrations/zoom";
+import SlackIntegrationCard from "@/integrations/slack";
+import { Button, Title } from "@mantine/core";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="pt-2">
-      <h1>Homepage</h1>
-
-      {/* <Button component={Link} href="/hello" className="m-4">
-        Next link button in Protected route
-      </Button> */}
-    </div>
+    <>
+      <Title mb={16}>Integrations</Title>
+      <ZoomIntegrationCard />
+      <SlackIntegrationCard />
+    </>
   );
 }
