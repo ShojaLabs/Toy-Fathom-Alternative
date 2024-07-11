1ec98b0712a224267f4cd93d86b77cb7aa9a3440
rangrik <pranav@shoja.app>
2024-07-11 22:23:10 +0530

    added new custom component

---
+++ src/components/Text/index.tsx
@@ -0,0 +1,7 @@
+import { Title as Heading, TitleProps } from "@mantine/core";
+
+export function Title(
+  props: TitleProps & React.ComponentPropsWithoutRef<"h1">,
+) {
+  return <Heading order={1} className="font-bold text-3xl" {...props} />
+}
