512a94abecac2ce4b6adf3f3af32260d67403eb9
rangrik <pranav@shoja.app>
2024-07-12 01:16:56 +0530

    Created a sample page

---
+++ src/app/(protected)/sample/page.tsx
@@ -0,0 +1,32 @@
+import { Title } from "@/components/Text";
+import { SampleCard } from "@/components/Card/sample";
+import { Paper, ScrollArea } from "@mantine/core";
+
+export default function Integrations() {
+  return (
+    <Paper className="h-full flex flex-col px-2">
+      <Title className="mb-2">Sample</Title>
+      <ScrollArea>
+        <div className="flex flex-wrap gap-4 pb-4 pt-2">
+          <SampleCard />
+          <SampleCard />
+          <SampleCard />
+          <SampleCard />
+          <SampleCard />
+          <SampleCard />
+          <SampleCard />
+          <SampleCard />
+          <SampleCard />
+          <SampleCard />
+          <SampleCard />
+          <SampleCard />
+          <SampleCard />
+          <SampleCard />
+          <SampleCard />
+          <SampleCard />
+        </div>
+      </ScrollArea>
+
+    </Paper>
+  );
+}
---
+++ src/components/Card/sample.tsx
@@ -0,0 +1,30 @@
+import { Card, Image, Text, Badge, Button, Group, CardSection } from '@mantine/core';
+
+export function SampleCard() {
+  return (
+    <Card withBorder padding="lg" bg="gray.0" className="w-80">
+      <CardSection>
+        <Image
+          src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+          // width={60}
+          className="h-40"
+          alt="Norway"
+        />
+      </CardSection>
+
+      <Group justify="space-between" mt="md" mb="xs">
+        <Text fw={500}>Norway Fjord Adventures</Text>
+        <Badge color="pink">On Sale</Badge>
+      </Group>
+
+      <Text size="sm" c="dimmed">
+        With Fjord Tours you can explore more of the magical fjord landscapes with tours and
+        activities on and around the fjords of Norway
+      </Text>
+
+      <Button color="blue" fullWidth mt="md" radius="md">
+        Book classic tour now
+      </Button>
+    </Card>
+  );
+}
