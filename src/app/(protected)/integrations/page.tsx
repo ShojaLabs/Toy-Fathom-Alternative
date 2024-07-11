509272b2ff9e81a73e6b1d85f3ba5609a40f0aff
rangrik <pranav@shoja.app>
2024-07-12 01:15:06 +0530

    scrollable integration page

--- src/app/(protected)/integrations/page.tsx
+++ src/app/(protected)/integrations/page.tsx
@@ -1,13 +1,32 @@
-import ZoomIntegrationCard from "@/integrations/zoom";
-import SlackIntegrationCard from "@/integrations/slack";
 import { Title } from "@/components/Text";
+import { IntegrationCard } from "@/components/Card/integration";
+import { Paper, ScrollArea } from "@mantine/core";
 
 export default function Integrations() {
   return (
-    <>
-      <Title>Integrations</Title>
-      <ZoomIntegrationCard />
-      <SlackIntegrationCard />
-    </>
+    <Paper className="h-full flex flex-col px-2">
+      <Title className="mb-2">Integrations</Title>
+      <ScrollArea>
+        <div className="flex flex-wrap gap-4 pb-4 pt-2">
+          <IntegrationCard />
+          <IntegrationCard />
+          <IntegrationCard />
+          <IntegrationCard />
+          <IntegrationCard />
+          <IntegrationCard />
+          <IntegrationCard />
+          <IntegrationCard />
+          <IntegrationCard />
+          <IntegrationCard />
+          <IntegrationCard />
+          <IntegrationCard />
+          <IntegrationCard />
+          <IntegrationCard />
+          <IntegrationCard />
+          <IntegrationCard />
+        </div>
+      </ScrollArea>
+
+    </Paper>
   );
 }
