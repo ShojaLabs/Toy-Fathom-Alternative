ad0594b98af432a1f0ec95405802577ba953976a
rangrik <pranav@shoja.app>
2024-07-12 02:36:06 +0530

    Added nav bar routes

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -16,7 +16,9 @@ export default function ProtectedAppLayout({
         <div className="fixed z-40 top-0 left-0 px-2 pt-[74px] pb-2 h-screen">
           <AppNavbar />
         </div>
-        <div className="fixed h-screen pt-[74px] pl-64 pb-2 pr-2 w-full">{children}</div>
+        <div className="fixed h-screen pt-[74px] pl-64 pb-2 pr-2 w-full">
+          {children}
+        </div>
       </main>
     </AuthProvider>
   );
---
+++ src/components/AppNavbar/routes/index.tsx
@@ -0,0 +1,56 @@
+import {
+  Icon,
+  IconAffiliate,
+  IconApple,
+  IconMailbox,
+  IconMathIntegrals,
+  IconProps,
+  IconSettings,
+  IconSparkles,
+} from "@tabler/icons-react";
+import { ForwardRefExoticComponent, RefAttributes } from "react";
+
+export type NavItemType = {
+  path: string;
+  icon: ForwardRefExoticComponent<IconProps & RefAttributes<Icon>>;
+  label: string;
+};
+
+export type AppNavItemsType = {
+  [key: string]: NavItemType;
+};
+
+const appNavItems: AppNavItemsType = {
+  home: {
+    path: "/",
+    icon: IconSparkles,
+    label: "Shoja",
+  },
+  dashboard: {
+    path: "/dashboard",
+    icon: IconMailbox,
+    label: "Dashboard",
+  },
+  org: {
+    path: "/org",
+    icon: IconAffiliate,
+    label: "Organization",
+  },
+  integrations: {
+    path: "/integrations",
+    icon: IconMathIntegrals,
+    label: "Integrations",
+  },
+  settings: {
+    path: "/settings",
+    icon: IconSettings,
+    label: "Settings",
+  },
+  sample: {
+    path: "/sample",
+    icon: IconApple,
+    label: "Sample",
+  },
+};
+
+export default appNavItems;
