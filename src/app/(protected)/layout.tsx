5aab81421b91ec56102ecccfc9f252dffd5fc1f4
rangrik <pranav@shoja.app>
2024-07-12 01:13:59 +0530

    fixed the layout

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -9,12 +9,14 @@ export default function ProtectedAppLayout({
 }>) {
   return (
     <AuthProvider>
-      <main className="h-screen p-2 flex flex-col">
-        <AppHeader />
-        <div className="flex grow pt-2">
-          <AppNavbar />
-          <div className="grow ml-2">{children}</div>
+      <main className="h-screen relative">
+        <div className="fixed z-50 top-0 left-0 p-2 w-full">
+          <AppHeader />
         </div>
+        <div className="fixed z-40 top-0 left-0 px-2 pt-[74px] pb-2 h-screen">
+          <AppNavbar />
+        </div>
+        <div className="fixed h-screen pt-[74px] pl-64 pb-2 pr-2 w-full">{children}</div>
       </main>
     </AuthProvider>
   );
