19a6c63244267a3307b7e90bc82ee20066a95a58
rangrik <pranav@shoja.app>
2024-07-14 00:12:21 +0530

    moved integrations callback to protected source tree as we need user info there

--- src/app/(protected)/callbacks/integrations/zoom/page.tsx
+++ src/app/(protected)/callbacks/integrations/zoom/page.tsx
@@ -3,10 +3,15 @@ import React, { useEffect } from "react";
 import { notifications } from "@mantine/notifications";
 import { useRouter } from "next/navigation";
 import { Loader } from "@mantine/core";
+import { useUserContext } from "supertokens-auth-react";
 
 function usePageLoadEffect() {
   const router = useRouter();
-  useEffect(() => {}, []); // Empty dependency array means this effect will only run once (on page load)
+  const userContext = useUserContext();
+
+  useEffect(() => {
+    console.log({ userContext });
+  }, [userContext]); // Empty dependency array means this effect will only run once (on page load)
 }
 
 function HandleCallback() {
