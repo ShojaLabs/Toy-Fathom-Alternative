cb8d9c6f4e4198e6ec2bbfe9072e4493d58cc9a3
rangrik <pranav@shoja.app>
2024-07-14 02:43:35 +0530

    created separate buttons to show different states of installation

--- src/app/(protected)/dashboard/integrations/_saasIntegrations/zoom/index.tsx
+++ src/app/(protected)/dashboard/integrations/_saasIntegrations/zoom/index.tsx
@@ -1,28 +1,55 @@
-"use client";
+import db from "@/services/db";
+import { eq } from "drizzle-orm";
+import ZoomInstallButton from "./zoomInstallButton";
+import { server_GetUserSession } from "@/supertokens/utils";
+import { recallZoomOauthCreds } from "@/services/db/schema/zoom";
+import {
+  IconAlertTriangleFilled,
+  IconCircleCheckFilled,
+} from "@tabler/icons-react";
 
-import React from "react";
-import InstallButton from "../../_components/installButton";
-import { useRouter } from "next/navigation";
+export const dynamic = "force-dynamic";
 
-function generateAuthUrl(redirectUri: string, zoomAppClientId: string): string {
-  const baseUrl = "https://zoom.us/oauth/authorize";
-  const queryParams = {
-    response_type: "code",
-    redirect_uri: redirectUri,
-    client_id: zoomAppClientId,
-  };
-  const queryString = new URLSearchParams(queryParams).toString();
-  return `${baseUrl}?${queryString}`;
+function ConnectedBanner() {
+  return (
+    <div className="flex gap-1 justify-center text-green-600">
+      <IconCircleCheckFilled size={24} />
+      Connected
+    </div>
+  );
 }
 
-export function ZoomAction() {
-  const router = useRouter();
-  const redirectUri = process.env.NEXT_PUBLIC_ZOOM_REDIRECT_URL!;
-  const zoomAppClientId = process.env.NEXT_PUBLIC_ZOOM_CLIENT_ID!;
-  const zoomAuthURL = generateAuthUrl(redirectUri, zoomAppClientId);
-  function onClick() {
-    console.log("Install Zoom", zoomAuthURL);
-    router.push(zoomAuthURL);
+function UnknownBanner() {
+  return (
+    <div className="flex gap-1 justify-center text-orange-400">
+      <IconAlertTriangleFilled size={24} /> Unknown please, refresh!
+    </div>
+  );
+}
+
+export async function ZoomAction() {
+  try {
+    const session = await server_GetUserSession();
+    const userId = session?.getUserId();
+
+    if (userId) {
+      const zoomOAuth = await db
+        .select({
+          id: recallZoomOauthCreds.id,
+        })
+        .from(recallZoomOauthCreds)
+        .where(eq(recallZoomOauthCreds.userId, userId));
+
+      if (zoomOAuth.length > 0) {
+        console.log("Zoom is installed");
+        return <ConnectedBanner />;
+      } else {
+        return <ZoomInstallButton />;
+      }
+    }
+  } catch (error) {
+    console.error("Can't find out if Zoom is installed", error);
   }
-  return <InstallButton onClick={onClick} />;
+
+  return <UnknownBanner />;
 }
