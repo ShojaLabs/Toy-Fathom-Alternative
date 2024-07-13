6b2340df2aae9e50ac1fd1346b359e0cdf568651
rangrik <pranav@shoja.app>
2024-07-14 02:42:45 +0530

    created separate get session function use with previous commit

--- src/supertokens/utils.ts
+++ src/supertokens/utils.ts
@@ -1,5 +1,15 @@
-import { cookies } from "next/headers";
+"server only";
+import { cookies, headers } from "next/headers";
+import { getSSRSession } from "supertokens-node/nextjs";
+import { ensureSuperTokensInit } from "./backend";
+
+ensureSuperTokensInit();
 
 export function getAccessToken(): string | undefined {
   return cookies().get("sAccessToken")?.value;
+}
+
+export async function server_GetUserSession() {
+  const { session } = await getSSRSession([...cookies().getAll()], headers());
+  return session;
 }
