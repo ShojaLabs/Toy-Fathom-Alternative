3de916b02f54e132705dbe3b8b947c35180de73a
rangrik <pranav@shoja.app>
2024-07-13 18:56:29 +0530

    configured drizzle kit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,10 +4,11 @@ export default defineConfig({
   schema: "./src/schema/*",
   out: "./drizzle",
   dbCredentials: {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: "127.0.0.1",
-    port: 5432,
-    database: process.env.APP_DB!,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // host: "127.0.0.1",
+    // port: 5432,
+    // database: process.env.APP_DB!,
+    url: process.env.APP_DB_URL!,
   },
 });
