68a5b3f7727594ff74a7566964b7f0fa937b5b89
rangrik <pranav@shoja.app>
2024-07-18 02:00:44 +0530

    Moved back the main page to `/`
    moved the page & root layout from `/dashboard` to `/`

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,9 +1,24 @@
 import AuthProvider from "@/supertokens/authProvider";
+import React from "react";
+import AppHeader from "@/components/AppHeader";
+import AppNavbar from "@/components/AppNavbar";
 
 export default function ProtectedAppLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <AuthProvider>{children}</AuthProvider>;
+  return <AuthProvider>
+    <main className="h-screen relative">
+      <div className="fixed z-50 top-0 left-0 p-2 w-full">
+        <AppHeader/>
+      </div>
+      <div className="fixed z-40 top-0 left-0 px-2 pt-[74px] pb-2 h-screen">
+        <AppNavbar/>
+      </div>
+      <div className="fixed h-screen pt-[74px] pl-64 pb-2 pr-2 w-full">
+        {children}
+      </div>
+    </main>
+  </AuthProvider>;
 }
