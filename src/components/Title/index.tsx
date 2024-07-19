c691c9d7834c1d2d3b20e4bf82b8f851aa15bb9f
rangrik <pranav@shoja.app>
2024-07-20 03:12:10 +0530

    updated title page to have the right line-height

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -5,5 +5,9 @@ import React from "react";
 export function Title(
   props: TitleProps & React.ComponentPropsWithoutRef<"h1">
 ) {
-  return <Heading order={1} className={clsx("font-bold text-2xl", props.className)} {...props} />
+  return (
+    <Heading
+      order={1}
+      className={clsx("font-bold text-2xl leading-10", props.className)} {...props}
+    />)
 }
