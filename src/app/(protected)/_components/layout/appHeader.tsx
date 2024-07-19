fda1d1b96e568de2b8162a91f973d0555f49ab18
rangrik <pranav@shoja.app>
2024-07-20 03:14:40 +0530

    Created page title component & added it to app header

--- src/app/(protected)/_components/layout/appHeader.tsx
+++ src/app/(protected)/_components/layout/appHeader.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { Paper } from '@mantine/core';
 import { Search } from "./search";
+import { PageTitle } from "./pageTitle";
 
 export function AppHeader() {
   return (
     <Paper
-      className="h-full p-2 flex justify-center"
+      className="h-full p-2 flex justify-between"
       bg="gray.0"
       withBorder
     >
-      <Search />
+      <div className="flex-1 pl-2">
+        <PageTitle />
+      </div>
+      <div className="flex-1">
+        <Search />
+      </div>
+      <div className="flex-1" />
     </Paper>
   );
 }
