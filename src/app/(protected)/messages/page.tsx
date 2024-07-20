074bb898bb08ec2c4b36e812510cdd4d16ecbea3
rangrik <pranav@shoja.app>
2024-07-20 17:04:49 +0530

    added dummy messages & meetings route

---
+++ src/app/(protected)/messages/page.tsx
@@ -0,0 +1,24 @@
+import { SampleCard } from "./_components/sample";
+
+export default function Integrations() {
+  return (
+    <div className="flex flex-wrap gap-2">
+      <SampleCard />
+      <SampleCard />
+      <SampleCard />
+      <SampleCard />
+      <SampleCard />
+      <SampleCard />
+      <SampleCard />
+      <SampleCard />
+      <SampleCard />
+      <SampleCard />
+      <SampleCard />
+      <SampleCard />
+      <SampleCard />
+      <SampleCard />
+      <SampleCard />
+      <SampleCard />
+    </div>
+  );
+}
