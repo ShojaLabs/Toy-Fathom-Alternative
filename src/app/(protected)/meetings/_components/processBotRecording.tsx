e68094749f3671778440032137f05a04f3f0053d
rangrik <pranav@shoja.app>
2024-07-21 00:54:28 +0530

    Updated the dummy meetings page to show the list of meetings a user has joined

---
+++ src/app/(protected)/meetings/_components/processBotRecording.tsx
@@ -0,0 +1,54 @@
+"use client"
+import React, { useState } from 'react';
+import { Button } from "@mantine/core";
+import { analyseBotMedia, retrieveBotDetailsFromRecall } from "@/app/(protected)/meetings/_actions/meeting_bots";
+import { notifications } from "@mantine/notifications";
+
+function ProcessBotRecording({ botId }: { botId: string }) {
+  const [loading, setLoading] = useState(false);
+  return (
+    <Button
+      variant="light"
+      className="mx-2"
+      loading={loading}
+      onClick={async () => {
+        setLoading(true);
+        console.log("Retrieving Bot Details and Starting Analysis...");
+        let retrieveStatus = await retrieveBotDetailsFromRecall(botId);
+        if (!retrieveStatus) {
+          notifications.show({
+            title: "Unable to retrieve bot information",
+            message: "Please try again",
+            color: "red",
+          });
+          setLoading(false);
+        } else {
+          notifications.show({
+            title: "Bot information retrieved successfully",
+            message: "Analysis started",
+            color: "green",
+          });
+          const analysisStatus = await analyseBotMedia(botId)
+          if (!analysisStatus) {
+            notifications.show({
+              title: "Unable to start media analysis",
+              message: "Please try again",
+              color: "red",
+            });
+          } else {
+            notifications.show({
+              title: "Media analysis started successfully",
+              message: "You will be notified once it's finished",
+              color: "green",
+            });
+          }
+          setLoading(false);
+        }
+      }}
+    >
+      Process
+    </Button>
+  );
+}
+
+export default ProcessBotRecording;
--- src/app/(protected)/meetings/page.tsx
+++ src/app/(protected)/meetings/page.tsx
@@ -1,4 +1,3 @@
-import { SampleCard } from "./_components/sample";
 import db from "@/services/db";
 import { recallMeetingBots } from "@/services/db/schema/meeting_bot";
 import { server_GetUserSession } from "@/supertokens/utils";
@@ -9,12 +8,14 @@ import Image from "next/image";
 import React from "react";
 import Images from "@/constants/images";
 import Link from "next/link";
-import { Button } from "@mantine/core";
+import { Button, Paper } from "@mantine/core";
+import ProcessBotRecording from "@/app/(protected)/meetings/_components/processBotRecording";
 
 export default async function Integrations() {
   const session = await server_GetUserSession();
   const userId = session?.getUserId();
   // TODO: Paginate
+  // TODO: Only fetch bots that are done recording (joinAt < today || null)
   let meetings = await db.select()
                          .from(recallMeetingBots)
                          .where(eq(recallMeetingBots.userId, userId!))
@@ -25,7 +26,7 @@ export default async function Integrations() {
     [day: string]: typeof meetings
   } = {};
   meetings.forEach((meeting) => {
-    const day = dayjs(meeting.joinAt).format('MMM DD, YYYY');
+    const day = meeting.joinAt ? dayjs(meeting.joinAt).format('MMM DD, YYYY') : "Recent";
     const currentDay = categorisedByDay[day]
     if (currentDay?.length > 0) {
       categorisedByDay[day].push(meeting);
@@ -33,24 +34,30 @@ export default async function Integrations() {
       categorisedByDay[day] = [meeting];
     }
   })
-
+  // TODO: Add a page to show that there are no meetings
+  // TODO: Add a component to show that there was an error fetching the meeting entries.
   return (
     <div className="flex flex-wrap gap-2">
       {Object.keys(categorisedByDay).map((day) => {
         return (
           <div
-            className="w-full"
+            className="w-full pb-8"
             key={day}
           >
             <h3 className="text-lg font-bold text-gray-800">{day}</h3>
-            <div className="mt-4 flex flex-col flex-wrap gap-6">
+            <div className="mt-4 grid grid-cols-2 gap-4">
               {categorisedByDay[day].map((meeting) => {
                 return (
-                  <div key={meeting.id}>
+                  <Paper
+                    bg="dark.6"
+                    className="p-2"
+                    key={meeting.id}
+                    withBorder
+                  >
                     <Image
                       src={Images[meeting.platform]}
-                      width={240}
-                      height={240}
+                      width={80}
+                      height={80}
                       alt="Zoom logo"
                       className="rounded-md mb-2"
                     />
@@ -59,7 +66,8 @@ export default async function Integrations() {
                         Details
                       </Button>
                     </Link>
-                  </div>
+                    <ProcessBotRecording botId={meeting.botId} />
+                  </Paper>
                 )
               })}
             </div>
