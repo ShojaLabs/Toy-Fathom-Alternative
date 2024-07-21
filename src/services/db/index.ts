0b9711edb72918c89db1851dbee2e69849a97e3c
rangrik <pranav@shoja.app>
2024-07-21 17:26:15 +0530

    feat: Add User schema to database service and define User table structure

--- src/services/db/index.ts
+++ src/services/db/index.ts
@@ -2,12 +2,14 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 import * as zoom from "./schema/zoom";
+import * as user from "./schema/user";
 
 // for query purposes
 const queryClient = postgres(process.env.APP_DB_URL!);
 const db = drizzle(queryClient, {
   schema: {
     ...zoom,
+    ...user,
   },
 });
 
