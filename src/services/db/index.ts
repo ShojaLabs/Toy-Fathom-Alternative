aa8d97eab1a6da65eed2aecc0ce5c39ac346272f
rangrik <pranav@shoja.app>
2024-07-22 00:21:13 +0530

    Added new schema for zoom oauth using recall

--- src/services/db/index.ts
+++ src/services/db/index.ts
@@ -1,15 +1,19 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-import * as zoom from "./schema/zoom";
 import * as user from "./schema/user";
+import * as integration from "./schema/integration";
+import * as installation from "./schema/installation";
+import * as zoom_oauth from "./schema/zoom_oauth";
 
 // for query purposes
 const queryClient = postgres(process.env.APP_DB_URL!);
 const db = drizzle(queryClient, {
   schema: {
-    ...zoom,
     ...user,
+    ...integration,
+    ...installation,
+    ...zoom_oauth,
   },
 });
 
