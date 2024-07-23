bd7a17fdbb650765c34d53a92df204daa37bc77a
rangrik <pranav@shoja.app>
2024-07-23 18:25:34 +0530

    refactor(database): Update schema for meeting_bot and meeting tables

--- src/services/db/index.ts
+++ src/services/db/index.ts
@@ -5,6 +5,8 @@ import * as user from "./schema/user";
 import * as integration from "./schema/integration";
 import * as installation from "./schema/installation";
 import * as zoom_oauth from "./schema/zoom_oauth";
+import * as meeting from "./schema/meeting";
+import * as meeting_bot from "./schema/meeting_bot";
 
 // for query purposes
 const queryClient = postgres(process.env.APP_DB_URL!);
@@ -14,6 +16,8 @@ const db = drizzle(queryClient, {
     ...integration,
     ...installation,
     ...zoom_oauth,
+    ...meeting,
+    ...meeting_bot,
   },
 });
 
