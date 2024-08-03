8b7e5a5f9a1cda7958e3e05478184cff26493cf6
rangrik <pranav@shoja.app>
2024-08-04 01:53:30 +0530

    Created an acceptable ui for showing summary & transcript.

--- src/app/(protected)/meetings/[id]/page.tsx
+++ src/app/(protected)/meetings/[id]/page.tsx
@@ -6,6 +6,7 @@ import Paths from "@/constants/paths";
 import { Title } from "@/ui-components/Title";
 import TranscriptSync from "./transcriptSync";
 import { Paper } from "@mantine/core";
+import { CloseButton } from "./closeButton";
 
 export default async function MeetingDetails({
   params: { id },
@@ -36,29 +37,57 @@ export default async function MeetingDetails({
     id,
     meeting?.meetingBot?.recallBotId!,
   );
+
+  // TODO: Use the following to make sure we send the information like a conversation in the transscript
+  // const { transcript, intelligence, speakerTimeline }: any =
+  //   meeting?.meetingBot;
+
+  const { intelligence }: any = meeting?.meetingBot;
+
   return (
     <div className="h-full">
-      <Title className="pb-3 text-2xl">{meeting?.meetingTitle}</Title>
-      {!meeting?.meetingBot?.transcriptProcessed && (
+      <div className="flex justify-between pb-3">
+        <Title className="text-2xl">{meeting?.meetingTitle}</Title>
+        <CloseButton />
+      </div>
+      {!meeting?.meetingBot?.transcriptProcessed ? (
         <TranscriptSync
           id={id}
           botId={meeting?.meetingBot?.recallBotId!}
           meetingDetailsUrl={meetingDetailsUrl}
         />
+      ) : (
+        <>
+          <Paper className="border p-4">
+            <h2 className="text-xl font-semibold mb-2">Summary</h2>
+            <p>{intelligence?.["assembly_ai.summary"]}</p>
+          </Paper>
+          <Paper className="border p-4 mt-4">
+            <h2 className="text-xl font-semibold mb-2">Transcript</h2>
+            {intelligence?.["assembly_ai.iab_categories_result"].results.map(
+              (sent: any, indx: number) => (
+                <p key={indx} className="pb-2">
+                  {sent.text}
+                </p>
+              ),
+            )}
+          </Paper>
+        </>
       )}
-      {meeting?.meetingBot?.transcriptProcessed && (
-        <Paper className="h-full border p-2">
-          {JSON.stringify(
-            {
-              transcript: meeting?.meetingBot?.transcript,
-              intelligence: meeting?.meetingBot?.intelligence,
-              speakerTimeline: meeting?.meetingBot?.speakerTimeline,
-            },
-            null,
-            2,
-          )}
-        </Paper>
-      )}
+
+      {/* {meeting?.meetingBot?.transcriptProcessed && ( */}
+      {/*   <Code block className="h-full border p-2"> */}
+      {/*     {JSON.stringify( */}
+      {/*       { */}
+      {/*         transcript, */}
+      {/*         intelligence, */}
+      {/*         speakerTimeline, */}
+      {/*       }, */}
+      {/*       null, */}
+      {/*       2, */}
+      {/*     )} */}
+      {/*   </Code> */}
+      {/* )} */}
     </div>
   );
 }
