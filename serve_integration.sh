23a629a4a7c3b26c1882c7c481a329ca4d8f40f4
rangrik <pranav@shoja.app>
2024-08-09 05:42:43 +0530

    updated the CORS config and created a script to serve the app on integration.shoja.ai

---
+++ serve_integration.sh
@@ -0,0 +1,4 @@
+while true; do
+  ssh -p 443 -R0:localhost:3000 -L4300:localhost:4300 -o StrictHostKeyChecking=no -o ServerAliveInterval=30 -t vbTlHBkLZFt@ap.a.pinggy.io x:https x:passpreflight
+  sleep 10
+done
