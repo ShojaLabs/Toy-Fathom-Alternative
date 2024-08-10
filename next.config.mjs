4a76539ff1e1d715938ac325ccfc8aea6df73e97
rangrik <pranav@shoja.app>
2024-08-10 14:48:06 +0530

    remove cors config

--- next.config.mjs
+++ next.config.mjs
@@ -9,30 +9,30 @@ const nextConfig = {
       },
     ];
   },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          }, // replace this your actual origin
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Authorization, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       // matching all API routes
+  //       source: "/api/:path*",
+  //       headers: [
+  //         { key: "Access-Control-Allow-Credentials", value: "true" },
+  //         {
+  //           key: "Access-Control-Allow-Origin",
+  //           value: "integration.shoja.ai,localhost:3000",
+  //         }, // replace this your actual origin
+  //         {
+  //           key: "Access-Control-Allow-Methods",
+  //           value: "GET,DELETE,PATCH,POST,PUT, OPTIONS",
+  //         },
+  //         {
+  //           key: "Access-Control-Allow-Headers",
+  //           value:
+  //             "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Authorization, Date, X-Api-Version",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
