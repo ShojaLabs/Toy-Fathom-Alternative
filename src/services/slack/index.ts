47dfcf5dfc389ffb74f6fbddb81fe35e4890bfb7
rangrik <pranav@shoja.app>
2024-08-19 03:38:22 +0530

    Created slack service

--- src/services/slack/index.ts
+++ src/services/slack/index.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
+import { App } from "@slack/bolt";
 
 const Slack = axios.create({
   baseURL: process.env.SLACK_API_BASE_PATH,
   headers: {
-    "Content-Type": "application/json",
+    "Content-Type": "application/json; charset=utf-8",
   },
 });
 export default Slack;
@@ -12,11 +13,30 @@ export const SlackApis = {
   authorise: () => "oauth.v2.access",
 };
 
-export async function authoriseSlack(code: string) {
-  return await Slack.post(SlackApis.authorise(), {
-    client_id: process.env.SLACK_CLIENT_ID,
-    client_secret: process.env.SLACK_CLIENT_SECRET,
-    code,
-    grant_type: "authorization_code",
+export async function authoriseSlack(code: string, userId: string) {
+  const slackApp = new App({
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    clientId: process.env.SLACK_CLIENT_ID,
+    clientSecret: process.env.SLACK_CLIENT_SECRET,
+    stateSecret: "test-state-secret-shoja-local-dev",
+    redirectUri: process.env.NEXT_PUBLIC_SLACK_REDIRECT_URL,
+    installerOptions: {
+      authVersion: "v2",
+      redirectUriPath: process.env.NEXT_PUBLIC_SLACK_REDIRECT_PATH,
+      metadata: userId,
+    },
   });
+
+  const params = {
+    client_id: process.env.SLACK_CLIENT_ID!,
+    client_secret: process.env.SLACK_CLIENT_SECRET!,
+    code: code,
+  };
+  // slackApp.processEvent()
+  console.log(params);
+  return await slackApp.client.oauth.v2.access(params);
 }
+
+// TODO: Add a factory function to get the Slack App object using bolt
+// There is an autorise function with App constructor read up on it.
+// https://slack.dev/bolt-js/concepts/event-listening
