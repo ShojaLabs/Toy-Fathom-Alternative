105d2fec5b77c2caba1cf423f19c557245dbb202
rangrik <pranav@shoja.app>
2024-08-19 03:40:12 +0530

    Handle events (simple template) WIP

--- src/app/api/v1/hooks/slack/route.ts
+++ src/app/api/v1/hooks/slack/route.ts
@@ -2,12 +2,15 @@ import { NextRequest, NextResponse } from "next/server";
 import JsonWebToken from "jsonwebtoken";
 import jws from "jws";
 import jwksClient from "jwks-rsa";
+import db from "@/services/db";
+import { and, eq } from "drizzle-orm";
+import { PlugsSlack } from "@/services/db/schema/plugs_slack";
 
 const client = jwksClient({
   jwksUri: `${process.env.NEXT_PUBLIC_SUPERTOKENS_WEBSITE_DOMAIN}${process.env.NEXT_PUBLIC_SUPERTOKENS_API_BASE_PATH}/jwt/jwks.json`,
 });
 
-// TODO: record these for better debugging
+// TODO: Handle this with app.processEvents
 export async function POST(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const token = searchParams.get("token")!;
@@ -24,38 +27,35 @@ export async function POST(request: NextRequest) {
     ) {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     } else {
+      console.log(
+        "Authorized!!",
+        // request.body,
+        // await request.text(),
+        // await request.json(),
+      );
       const body = await request.json();
+      const event = body?.event;
+      const teamId = body?.team_id;
+      const slackUserId = event?.user;
 
-      return NextResponse.json(body.challenge, {
+      const slackPlug = await db.query.PlugsSlack.findFirst({
+        where: and(
+          eq(PlugsSlack.slackUserId, slackUserId),
+          eq(PlugsSlack.teamId, teamId),
+        ),
+        columns: {
+          userId: true,
+          botAccessToken: true,
+          userAccessToken: true,
+        },
+      });
+      console.log({ slackPlug });
+      return NextResponse.json(body?.challenge || "OK", {
         status: 200,
       });
-      // console.log("request", body);
-      //
-      // const { data, event } = body;
-      //
-      // // Handle Bot Web Hooks
-      // if (event === "bot.status_change") {
-      //   const { bot_id, status } = data;
-      //   if (status.code == "done") {
-      //     analyseBotMedia(bot_id);
-      //   } else if (status.code == "analysis_done") {
-      //     storeTranscriptData(bot_id);
-      //   }
-      // }
-      // // Handle Calendar Web Hooks
-      // else if (event === "calendar.sync_events") {
-      //   const { calendar_id, last_updated_ts } = data;
-      //   console.log("calendar synced", calendar_id, last_updated_ts);
-      //   syncCalendarEvents(calendar_id, last_updated_ts);
-      // } else if (event === "calendar.update") {
-      //   const { calendar_id } = data;
-      //   updateCalendar(calendar_id);
-      // }
-      //
-      // return NextResponse.json({ success: "Hook executed" }, { status: 200 });
     }
-  } catch (e) {
-    console.log("Error In POST HOOKs api", e);
+  } catch (e: any) {
+    console.error("Error In POST HOOKs api", e.message);
     return NextResponse.json(
       { error: "Internal Server Error" },
       { status: 500 },
